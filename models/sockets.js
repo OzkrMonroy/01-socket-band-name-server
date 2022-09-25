65335cc6b905a5ad3e7b4cf98bbe85f039b73e15
Oscar Monroy <oscarmonroy657@gmail.com>
2022-09-24 21:21:38 -0600

    Now the socket sends the band list when a user is connected.

--- models/sockets.js
+++ models/sockets.js
@@ -1,24 +1,15 @@
+const BandList = require("./band-list");
+
 class Sockets {
   constructor(io) {
     this.io = io;
-    this.socketEvents()
+    this.bandList = new BandList();
+    this.socketEvents();
   }
 
   socketEvents() {
     this.io.on("connection", (socket) => {
-      console.log("Client is connected, with id:" + socket.id);
-      socket.emit("welcome-message", {
-        message: "Welcome to the server",
-        date: new Date(),
-      });
-
-      socket.on("client-message", (data) => {
-        console.log("Message received: ", data);
-      });
-      socket.on("chat-message-fe", (data) => {
-        console.log("Message received from chat: ", data);
-        this.io.emit("chat-message-bk", data);
-      });
+      socket.emit("current-bands", this.bandList.getBands());
     });
   }
 }
